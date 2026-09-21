0a1a44e04af6be3c16a4a010acee0f26a1e54ad5
Silvio Braendle <silvio@silviobraendle.ch>
2026-09-21 11:24:23 +0200

    Undo testing.

--- services/properties/test/app.test.ts
+++ services/properties/test/app.test.ts
@@ -25,7 +25,7 @@ describe('properties app', () => {
     expect(response.status).toBe(200);
     await expect(response.json()).resolves.toEqual({
       status: 'ok',
-      service: 'properties Hello World',
+      service: 'properties',
       version: 'test',
     });
   });
